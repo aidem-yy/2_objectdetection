eeceded53252d8061d8a9f6bcb0317ce10485453
aidem-yy <suinikotai@yahoo.co.jp>
2023-11-12 02:25:35 +0900

    学習済みモデルの階層変更

--- obejectdetection_app.py
+++ obejectdetection_app.py
@@ -18,7 +18,7 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 #学習済みモデルをロード
-model = YOLO('./LFS/best.pt')
+model = YOLO('./best.pt')
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
